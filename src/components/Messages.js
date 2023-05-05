ff4a662dffb838f54a14916f8a72c0c129c7d167
antoniobiskupec <antoniobiskupec1@gmail.com>
2023-05-05 21:51:10 +0200

    chat app working, need to edit css

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,37 +1,37 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import { useRef, useEffect } from "react";
 
 function Messages({ messages, currentMember }) {
   const bottomRef = useRef(null);
 
+  //Called useEffect so when messages update, scroll behavior can trigger
   useEffect(() => {
     bottomRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
-  // function that renders one message
+  //Main function and logic for displaying messages while mapping through messages array
   function renderMessage(message) {
-    // extracting message and member
-    const { member, text } = message;
-
-    // check if i sent the message
-    const myMessage = member.id === currentMember.id;
-
-    // setting the class depenging on who sent the message
-    const className = myMessage
+    const { data, member } = message;
+    const myMessages = member.id === currentMember.id;
+    const className = myMessages
       ? "Messages-message currentMember"
       : "Messages-message";
-
     return (
-      <li className={className} key={Math.random}>
-        <span className='avatar' />
+      <li className={className} key={Math.random()}>
+        <span
+          className='avatar'
+          style={{ backgroundColor: member.clientData.color }}
+        />
         <div className='Message-content'>
           <div className='username'>{member.clientData.username}</div>
-          <div className='text'>{text}</div>
+          <div className='text' ref={bottomRef}>
+            {data}
+          </div>
         </div>
       </li>
     );
   }
 
-  // list render of messages
   return (
     <ul className='Messages-list'>{messages.map((m) => renderMessage(m))}</ul>
   );
